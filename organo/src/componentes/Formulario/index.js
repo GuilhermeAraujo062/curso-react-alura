47e9cf81772decad3c964173e79b34cc2be51480
Guilherme Araujo <99432207+GuilhermeAraujo062@users.noreply.github.com>
2024-09-23 09:53:15 -0300

    Formulario completo

--- organo/src/componentes/Formulario/index.js
+++ organo/src/componentes/Formulario/index.js
@@ -1,14 +1,34 @@
 import CampoTexto from '../CampoTexto/CampoTexto'
 import './Formulario.css'
+import ListaSuspensa from '../ListaSuspensa';
+import Botao from '../Botao'
 
 const Formulario = () => {
+
+    const times = [
+        'Programação',
+        'Front-End',
+        'Data Science',
+        'Devops',
+        'UX e Desing',
+        'Mobile',
+        'Inovação e Gestão'
+      ];
+
+      const aoSalvar = (evento) => {
+        evento.preventDefault();
+        console.log('Submetido');
+      }
+
     return (
         <section className="formulario">
-            <form>
+            <form onSubmit={aoSalvar}>
                 <h2>Preecha os dados para criar o card do colaborador</h2>
-                <CampoTexto label="Nome" placeholder="Digite seu nome" />
-                <CampoTexto label="Cargo" placeholder="Digite seu cargo" />
+                <CampoTexto obrigatorio={true} label="Nome" placeholder="Digite seu nome" />
+                <CampoTexto obrigatorio={true} label="Cargo" placeholder="Digite seu cargo" />
                 <CampoTexto label="Imagem" placeholder="Digite o endereço da imagem" />
+                <ListaSuspensa obrigatorio={true} label='Time' itens={times}/>
+                <Botao>Criar Card</Botao>
             </form>
         </section>
     );
