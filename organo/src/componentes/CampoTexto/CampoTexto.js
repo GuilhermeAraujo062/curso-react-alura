8b4b9f6ba1237275179a765fab211ce6c9a47a96
Guilherme Araujo <99432207+GuilhermeAraujo062@users.noreply.github.com>
2024-09-24 08:28:48 -0300

    Formulario

--- organo/src/componentes/CampoTexto/CampoTexto.js
+++ organo/src/componentes/CampoTexto/CampoTexto.js
@@ -1,12 +1,19 @@
-import './CampoTexto.css'
+import './CampoTexto.css';
 
 const CampoTexto = (props) => {
+
+    // let valor ='Guilherme';
+
+    const aoDigitando = (evento) => {
+        props.aoAlterado(evento.target.value);
+    }
+
     return (
         <div className="campo-texto">
             <label>{props.label}</label>
-            <input required={props.obrigatorio} placeholder={props.placeholder}></input>
+            <input value={props.valor} onChange={aoDigitando} required={props.obrigatorio} placeholder={props.placeholder}></input>
         </div>
-    )
+    );
 }
 
-export default CampoTexto
+export default CampoTexto;
