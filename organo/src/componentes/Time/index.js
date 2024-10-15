ef6a1dafa0cb3019432d9aa94b724b3b8c3bcfc0
Guilherme Araujo <99432207+GuilhermeAraujo062@users.noreply.github.com>
2024-10-15 08:24:43 -0300

    Update index.js

--- organo/src/componentes/Time/index.js
+++ organo/src/componentes/Time/index.js
@@ -7,7 +7,7 @@ const Time = (props) => {
         (props.colaboradores.length > 0) && <section className='time' style={{ backgroundColor: props.corSecundaria }}>
             <h3 style={{ borderColor: props.corPrimaria }}>{props.nome}</h3>
             <div className='cc'>
-                {props.colaboradores.map(colaborador => <Colaborador key={colaborador.nome} nome={colaborador.nome} cargo={colaborador.cargo} imagem={colaborador.imagem} corDeFundo={props.corPrimaria}/> )}
+                {props.colaboradores.map(colaborador => <Colaborador key={colaborador.nome} nome={colaborador.nome} cargo={colaborador.cargo} imagem={colaborador.imagem} corDeFundo={props.corPrimaria} aoDeletar={aoDeletar}/> )}
             </div>
             
         </section>
