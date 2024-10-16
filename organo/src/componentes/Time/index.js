e80e349877e5a2c0215cc6c7aec74b70b8216a56
Guilherme Araujo <99432207+GuilhermeAraujo062@users.noreply.github.com>
2024-10-16 08:22:04 -0300

    botão Deletar

--- organo/src/componentes/Time/index.js
+++ organo/src/componentes/Time/index.js
@@ -7,7 +7,14 @@ const Time = (props) => {
         (props.colaboradores.length > 0) && <section className='time' style={{ backgroundColor: props.corSecundaria }}>
             <h3 style={{ borderColor: props.corPrimaria }}>{props.nome}</h3>
             <div className='cc'>
-                {props.colaboradores.map(colaborador => <Colaborador key={colaborador.nome} nome={colaborador.nome} cargo={colaborador.cargo} imagem={colaborador.imagem} corDeFundo={props.corPrimaria} aoDeletar={aoDeletar}/> )}
+                {props.colaboradores.map(colaborador => <Colaborador 
+                    key={colaborador.nome} 
+                    nome={colaborador.nome} 
+                    cargo={colaborador.cargo} 
+                    imagem={colaborador.imagem} 
+                    corDeFundo={props.corPrimaria} 
+                    aoDeletar={props.aoDeletar}/> 
+                )}
             </div>
             
         </section>
