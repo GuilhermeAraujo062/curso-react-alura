666a93d41a675ff63d4294173a3c66b174758fc6
Guilherme Araujo <99432207+GuilhermeAraujo062@users.noreply.github.com>
2024-10-23 08:48:06 -0300

    instalando uuid

--- organo/src/App.js
+++ organo/src/App.js
@@ -3,35 +3,43 @@ import Banner from './componentes/Banner/Banner';
 import Formulario from './componentes/Formulario';
 import Time from './componentes/Time'
 import Footer from './componentes/Footer';
+import { v4 as uuidv4 } from 'uuid';
 
 function App() {
 
   const [times, setTimes] = useState([
     {
+      id: uuidv4(),
       nome: 'Programação',
       cor: '#57C278',
     },
     {
+      id: uuidv4(),
       nome: 'Front-End',
       cor: '#82CFFA',
     },
     {
+      id: uuidv4(),
       nome: 'Data Science',
       cor: '#A6D157',
     },
     {
+      id: uuidv4(),
       nome: 'Devops',
       cor: '#E06B69',
     },
     {
+      id: uuidv4(),
       nome: 'UX e Desing',
       cor: '#D86EBF',
     },
     {
+      id: uuidv4(),
       nome: 'Mobile',
       cor: '#FFBA05',
     },
     {
+      id: uuidv4(),
       nome: 'Inovação e Gestão',
       cor: '#FF8A29',
     },
@@ -47,9 +55,9 @@ function App() {
     console.log('deletando colaborador');
   }
 
-  function mudarCorDoTime(cor, nome) {
+  function mudarCorDoTime(cor, id) {
     setTimes(times.map(time => {
-      if(time.nome === nome) {
+      if(time.nome === id) {
         time.cor = cor;
       }
       return time;
