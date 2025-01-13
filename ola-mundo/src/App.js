b260048fe1cce09c92b9f3ce3578b151341c91a8
Guilherme Araujo <99432207+GuilhermeAraujo062@users.noreply.github.com>
2025-01-13 09:11:25 -0300

    Inicio

--- ola-mundo/src/App.js
+++ ola-mundo/src/App.js
@@ -1,8 +1,16 @@
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Inicio from "./paginas/Inicio";
+import SobreMim from "./paginas/SobreMim";
 
 function App() {
   return (
-    <h1>Ola Mundo</h1>
+    <BrowserRouter>
+      <Routes>
+        <Route  path="/" element={<Inicio/>} />
+        <Route path="/SobreMim" element={<SobreMim/>} />
+        <Route path="*" element={<div>Página não encontrada</div>} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
